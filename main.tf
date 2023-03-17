b27b82d39b4ee2f8d7dca464a1f6ca950adcc8e7
cjoblin <127150666+cjoblin@users.noreply.github.com>
2023-03-17 15:46:10 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,3 +48,46 @@ resource "azurerm_dns_zone" "dns-subdomain" {
   resource_group_name = azurerm_resource_group.rg-dnstest.name
 }
 
+resource "azurerm_role_definition" "dns-subdomain-writer" {
+  role_definition_id = "00000000-0000-0000-0000-000000000000"
+  name               = "dns-subdomain-writer"
+  scope              = data.azurerm_dns_zone.dns-testdomain.id
+
+  permissions {
+    actions     = ["Microsoft.Network/dnsZones/A/write", 
+	    	   "Microsoft.Network/dnszones/A/delete",
+	    	   "Microsoft.Network/dnszones/AAAA/write",
+	    	   "Microsoft.Network/dnszones/AAAA/delete",
+	    	   "Microsoft.Network/dnszones/CAA/write",
+	    	   "Microsoft.Network/dnszones/CAA/delete",
+	    	   "Microsoft.Network/dnszones/CNAME/write",
+	    	   "Microsoft.Network/dnszones/CNAME/delete",
+	   	   "Microsoft.Network/dnszones/MX/write",
+	    	   "Microsoft.Network/dnszones/MX/delete",
+	    	   "Microsoft.Network/dnszones/NS/write",
+	    	   "Microsoft.Network/dnszones/NS/delete",
+	    	   "Microsoft.Network/dnszones/PTR/write",
+	    	   "Microsoft.Network/dnszones/PTR/delete",
+	    	   "Microsoft.Network/dnszones/SOA/write",
+	    	   "Microsoft.Network/dnszones/SRV/write",
+	    	   "Microsoft.Network/dnszones/SRV/delete",
+	    	   "Microsoft.Network/dnszones/TXT/write",
+	    	   "Microsoft.Network/dnszones/TXT/delete",
+    		   "Microsoft.Network/dnszones/all/read"
+    		   ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_dns_zone.dns-testdomain.id,
+    data.azurerm_dns_zone.dns-subdomain.id
+  ]
+}
+
+resource "azurerm_role_assignment" "dns-subdomain-np1-assign" {
+  name               = "00000000-0000-0000-0000-000000000000"
+  scope              = data.azurerm_dns_zone.dns-subdomain.id
+  role_definition_id = azurerm_role_definition.dns-subdomain-writer.role_definition_resource_id
+  principal_id       = "17328cd2-4ba0-4c07-bb22-6d3154362bf5"
+  #need to integrate with user creation later
+}
