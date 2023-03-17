179ed1cefda361b9870b53aec8eab4fd11b1f171
cjoblin <127150666+cjoblin@users.noreply.github.com>
2023-03-17 15:08:35 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,18 +33,26 @@ provider "azurerm" {
   client_secret   = var.deploymentSecret
 }
 
-resource "azurerm_resource_group" "rg-dnstest" {
-  name     = "rg-dnstest"
-  location = "Australia East"
-}
+#resource "azurerm_resource_group" "rg-dnstest" {
+#  name     = "rg-dnstest"
+#  location = "Australia East"
+#}
 
-resource "azurerm_dns_zone" "dns-testdomain" {
-  name                = "testdomain.com"
-  resource_group_name = azurerm_resource_group.rg-dnstest.name
-}
+#resource "azurerm_dns_zone" "dns-testdomain" {
+#  name                = "testdomain.com"
+#  resource_group_name = azurerm_resource_group.rg-dnstest.name
+#}
 
-resource "azurerm_dns_zone" "dns-subdomain" {
-  name                = "sub.testdomain.com"
+#resource "azurerm_dns_zone" "dns-subdomain" {
+#  name                = "sub.testdomain.com"
+#  resource_group_name = azurerm_resource_group.rg-dnstest.name
+#}
+
+resource "azurerm_dns_a_record" "example" {
+  name                = "testnp1"
+  zone_name           = azurerm_dns_zone.dns-subdomain.name
   resource_group_name = azurerm_resource_group.rg-dnstest.name
+  ttl                 = 300
+  records             = ["10.0.180.17"]
 }
 
