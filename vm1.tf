55c86c72c20a456c20c3ddf1ece8572be90f6358
cjoblin <127150666+cjoblin@users.noreply.github.com>
2023-03-23 12:31:02 +1300

    Create vm1.tf

---
+++ vm1.tf
@@ -0,0 +1,39 @@
+resource "azurerm_network_interface" "vm1-nic" {
+  name                = "vm1-nic"
+  location            = azurerm_resource_group.rg-dnstest.location
+  resource_group_name = azurerm_resource_group.rg-dnstest.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = azurerm_resource_group.rg-dnstest.name
+  location            = azurerm_resource_group.rg-dnstest.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm1-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
