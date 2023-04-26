eb2076ee4c4926926e918a0e893b92c052150fc4
cjoblin <127150666+cjoblin@users.noreply.github.com>
2023-04-26 13:33:20 +1200

    Update vm1.tf

--- vm1.tf
+++ vm1.tf
@@ -10,32 +10,32 @@ resource "azurerm_network_interface" "vm1-nic" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm4" {
-  name                = "vm1"
-  resource_group_name = azurerm_resource_group.rg-dnstest.name
-  location            = azurerm_resource_group.rg-dnstest.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.vm1-nic.id,
-  ]
+#resource "azurerm_linux_virtual_machine" "vm4" {
+#  name                = "vm1"
+#  resource_group_name = azurerm_resource_group.rg-dnstest.name
+#  location            = azurerm_resource_group.rg-dnstest.location
+#  size                = "Standard_F2"
+#  admin_username      = "adminuser"
+#  network_interface_ids = [
+#    azurerm_network_interface.vm1-nic.id,
+#  ]
   
-  admin_password = "P@ssw0rd44"
-  disable_password_authentication = false
+#  admin_password = "P@ssw0rd44"
+#  disable_password_authentication = false
 #  admin_ssh_key {
 #    username   = "adminuser"
 #    public_key = file("~/.ssh/id_rsa.pub")
 #  }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#  os_disk {
+#    caching              = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-}
+#  source_image_reference {
+#    publisher = "Canonical"
+#    offer     = "UbuntuServer"
+#    sku       = "16.04-LTS"
+#    version   = "latest"
+#  }
+#}
